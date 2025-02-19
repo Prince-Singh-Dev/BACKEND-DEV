8bd52fabf215045eaf0e1eb345917d7ae2563dd4
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-19 09:26:30 +0530

    DAY 1

---
+++ Lecture1/script.js
@@ -0,0 +1,11 @@
+//fundamentals of javascript
+// arrays and objects
+// function return
+// async js coding 
+
+var arr = [1,2,3,4];
+// need to know in : array foreach map filter find indexOF  
+
+arr.forEach(function(val){
+    console.log(val + "helo");
+})
