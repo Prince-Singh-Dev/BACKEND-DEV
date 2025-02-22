39a35675ddaf89e48d53c0b6f3684394564e08de
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-22 06:38:47 +0530

    DAY 1

--- Lecture1/script.js
+++ Lecture1/script.js
@@ -1,7 +1,4 @@
-//fundamentals of javascript
-// arrays and objects
-// function return
-// async js coding 
+//fundamentals of javascript and arrays 
 
 var arr = [1,2,3,4];
 // need to know in : array foreach map filter find indexOF  
@@ -12,3 +9,71 @@ arr.forEach(function(val){
 
 //mapping function for array ; making one more array in comparison of previous array 
 
+var newarr = arr.map(function(val){
+    return val*3 ;
+})
+
+console.log(newarr);
+
+//filter functon for array : filter out the element from array using conditions
+
+//Printing all the elements that are greater than 3
+
+var ans = arr.filter(function (val){
+    if (val>3) {return true;}
+        else return false;
+})
+
+console.log(ans);
+
+//find function
+
+var answer = arr.find(function(val){
+    if(val===2) return val;
+})
+
+console.log(answer);
+
+//index off function : finding the elements on the basis of its indexing 
+
+let x = arr.indexOf(12);
+console.log(x);
+
+let y = arr.indexOf(1);
+console.log(y);
+
+//Fundamentals of objects in javascript
+
+//Key value pairs 
+
+var obj = {
+    name : "shivangi" , 
+    age : 26 
+}
+
+Object.freeze(obj); //Freezing the object value , so that value will be immutable
+var names = obj['name']; //acessing name from array
+const X = obj.age = 25; //changing the age of object
+console.log(obj ,names,X);
+
+// Funcrions return 
+
+function abcd(){
+    return 12;
+}
+
+var ans = abcd();
+console.log(ans);
+
+//Async of JAVASCRIPT coding 
+
+// Line by Line code will run ; called as synchronous
+// the code that is of async nature will be sent in side stack and the code that is sync in nature that will be run and after the main stack (sync code is completed ) then move the side stack code to main stack and then run it 
+
+async function abcde (){
+    var blob = await fetch(`https://randomuser.me/api/`)
+    var acc = blob.json();
+    console.log(acc);
+}
+
+abcde();
