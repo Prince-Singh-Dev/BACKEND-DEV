884f89d58678815c4b4b79f63ed62c5ed73a1554
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-24 20:41:33 +0530

    DAY 3

--- Lecture3/script.js
+++ Lecture3/script.js
@@ -1,10 +1,23 @@
 // NPM Understanding 
-
-// npm -- Previous name { node package manager } 
-
 // Installing and Uninstalling anything basics and advanced 
 // Understanding node_modules
 // Dependencies
 // DevDependencies
 // Scripts - understanding default scripts PATH and custom Scripts 
 
+
+// NPM : Node Package Manager
+// The things that are pre installed in node js are known as modules 
+// The things that are downloaded through npm are known as Pacakages 
+
+// For example : We have to make an application fro visually disabled where the app will read the things louder for those persons 
+//Now for this app , we need text to speech , thus google already make it for us on NPM named as "Accessibility" Packages 
+
+// Installing Packages from NPM 
+// Synatx : install -> npm i package_name 
+
+// Uninstalling Packages from NPM 
+// Syntax : npm uninstall package_name 
+
+// Installing any specific version of Package via NPM 
+// Syntax : npm i accessibility@3.8.9
