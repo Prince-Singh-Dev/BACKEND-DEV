dd714bde9a992b16150e54a7c1972381d15ea765
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-24 06:00:09 +0530

    DAY 3

---
+++ Lecture3/script.js
@@ -0,0 +1,6 @@
+// NPM Understanding 
+// Installing and Uninstalling anything basics and advanced 
+// Understanding node_modules
+// Dependencies
+// DevDependencies
+// Scripts - understanding default scripts PATH and custom Scripts 
