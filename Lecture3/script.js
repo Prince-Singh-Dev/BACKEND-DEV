d8b807b545deaa7c53e54e2b1124667659775003
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-24 20:04:40 +0530

    DAY 3

--- Lecture3/script.js
+++ Lecture3/script.js
@@ -1,6 +1,10 @@
 // NPM Understanding 
+
+// npm -- Previous name { node package manager } 
+
 // Installing and Uninstalling anything basics and advanced 
 // Understanding node_modules
 // Dependencies
 // DevDependencies
 // Scripts - understanding default scripts PATH and custom Scripts 
+
