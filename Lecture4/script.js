76a8ba36072b2305d612b04dacbb13b23f127fc1
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-27 06:09:33 +0530

    Initial Commit

--- Lecture4/script.js
+++ Lecture4/script.js
@@ -1,1 +1,45 @@
-//Welcome to Lectue 4 
+// Express.js Framework
+
+
+// Introduction to Express
+
+
+// MERN Stack --> MongoDB , Express.js , React.js , Node.js 
+// Express.js is a npm package , It is an framework 
+// Framework --> when any technology says , how to do to work { Framework generally give the flow of work execution } 
+// It manages everything from receving the request to giving back the response 
+// For example : sending request to Youtube to start a video , then request is processed and given a response in the form of video
+// Error Handling 
+
+
+// Setting up a basic Express application
+
+
+const express = require('express') //requring express package and complete functionality of express is transfered to express variables
+const app1 = express()  // complete express power tranfered to app
+
+app1.get('/', function (req, res) { 
+  res.send('Hello World')
+})
+
+app1.listen(3000)
+
+
+// Routing : Creating different Routes { youtube.com/hello --> here hello is the route }
+
+
+const express = require('express');
+const app = express();
+
+app.get("/",function(req,res){
+    res.send("Love you Shivangi");
+})
+
+app.get("/profile",function(req,res){
+    res.send("Love you 2");
+})
+
+app.listen(3000);
+
+
+// Middleware 
