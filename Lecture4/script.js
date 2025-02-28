46514de6dae31353ce88f7bb52d0be8c58fc4776
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-02-28 06:05:29 +0530

    DAY 4

--- Lecture4/script.js
+++ Lecture4/script.js
@@ -27,9 +27,18 @@ app1.listen(3000)
 
 // Routing : Creating different Routes { youtube.com/hello --> here hello is the route }
 
+// Middleware : The request that we send to facebook for photo , first it would given to middleware than after that request move to routes and response 
+// Whenever server accept the request , from their to reaching to the route , if we stop the request in mid and we do something , then this element is known as Middleware
+// for example : we send a request to youtube to download a video then using the middleware we can print hello in mid of request then request can be pass on to the route and response will be generated in terms of downloading video 
+
 
 const express = require('express');
 const app = express();
+
+app.use(function(req,res,next){ //first request will come here and then next will perform 
+  console.log("middleware is running"); //middleware would be printed on terminal and console
+  next();
+}); //Using middleware , before any requests it will run 
 
 app.get("/",function(req,res){
     res.send("Love you Shivangi");
@@ -39,9 +48,14 @@ app.get("/profile",function(req,res){
     res.send("Love you 2");
 })
 
+app.get("/about",function(req,res){
+  res.send("my fav shivangi joshi");
+})
+
+app.use((err,req,res,next) => {     //error handling 
+  console.error(err.stack)
+  res.status(500).send('Something Broke !')
+})
+
 app.listen(3000);
 
-
-// Middleware : The request that we send to facebook for photo , first it would given to middleware than after that request move to routes and response 
-// Whenever server accept the request , from their to reaching to the route , if we stop the request in mid and we do something , then this element is known as Middleware
-// for example : we send a request to youtube to download a video then using the middleware we can print hello in mid of request then request can be pass on to the route and response will be generated in terms of downloading video 
