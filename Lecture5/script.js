abe022d5cff8e82daaed34ee5aa1157831d9f78a
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-04-16 05:04:45 +0530

    Day 5

--- Lecture5/script.js
+++ Lecture5/script.js
@@ -4,6 +4,9 @@
 const express = require('express');
 const app = express();
 
+//We have a google form , filling emal , and name , data and after clicking on submit , it goes on the server in blob (unreadable stream , hexadecimal in short) mode
+
+// you have sent a plan text but server got a blob so to handle this and making it in readable form , we use these two lines 
 app.use(express.json()); //using a middle ware
 app.use(express.urlencoded({extended:true}));
 
@@ -17,4 +20,4 @@ app.get("/about",function(req,res){
 
 //Session - Cookie 
 //We can store any data on the browser and whenever we do a request for any data in backend then the data that saved on frontend will move to the backend 
-//Cookie : saved data on browser used in authentication purpose mainly  
+//Cookie : saved data on browser used in authentication purpose mainly ..
