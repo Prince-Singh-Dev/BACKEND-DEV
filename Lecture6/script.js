3c44555862d49c813beb1cea3689b93b53b4853c
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-04-21 21:09:24 +0530

    DAY 7

--- Lecture6/script.js
+++ Lecture6/script.js
@@ -10,19 +10,36 @@
 //How to get data coming from frontend at backend route
 
 const express = require('express');
+const path = require('path'); // <-- Add this
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({Extended : true}));
 app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine','ejs');
 
-app.get("/",function(req,res){
-    res.render("index.ejs");
+//Dynamic Routing : We saw alot of routes in our life , in those routs only one thing is changed .. for example 
+// /author/harsh
+// /author/harshita
+// /author/Prince
+// /author/Shivangi
+// here one thing is common i.e. author and one thing is different i.e. harsh,harshita,Prince,Shivangi
+
+// app.get("/profile/:username",function(req,res){            //Created my own url named as localhost:3000/profile/shivangi
+//     res.send("Love from Prince to SHIVANGI JOSHI 3000");
+//     //finding which value is coming despite on the place of username
+//     res.send(req.params.username);
+// });
+
+//Handling the username from backend , printing it on frontend and sending some message over frontend also ...
+
+app.get("/profile/:username",function(req,res){
+    res.send(`Love you 3000 , ${req.params.username}`);
 });
+
+// Now to make it dynamic URL ... Now realise which part is dynamic and after realising , include colon : in that route , i.e
+// localhost:3000/profile/shivangi  .. here shivangi is dynamic so add colon before it 
 
 app.listen(3000,function(){
     console.log("I love Shivangi Joshi");
 })
 
-//Now From Dynamic Routing I have to start ...
