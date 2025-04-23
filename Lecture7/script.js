2a67107d4ae6f81e2d6e52c670e734ef453820f6
Prince-Singh-Dev <princesingh6173@gmail.com>
2025-04-23 05:30:55 +0530

    Day 8

--- Lecture7/script.js
+++ Lecture7/script.js
@@ -1,1 +1,18 @@
 // Revising complete Key concepts that we have done till now in Back-End series
+
+// to create a package json file : terminal { npm init -y }
+// includes main file : index.js
+// installinh express and nodemon pacakage : npm i express
+
+// const express = require('express');
+// const app + express();
+
+// app.set("view engine","ejs");
+// app.use(express.json());
+// app.use(express.urlencoded({extended : true }));
+// app.use(express.static(path.join(__dirname,"public")));
+
+// app.get('/',function(req,res){
+// app.send("welcome");
+// })
+// app.listen(3000);
